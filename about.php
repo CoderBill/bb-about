d98eab5fabe2b14405e1278dfac01d1821831105
William Cole <bill@codemasterbill.com>
2017-10-14 04:21:40 -0700

    All files for the project committed.

---
+++ about.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/about.css"/>
+    <title>Document</title>
+</head>
+<body>
+<div id="content" class="column">
+
+<div id="navButtons">
+<a href="#" id="goPrev" class="button">&#171; <span>Previous</span></a>
+<a href="#" id="goNext" class="button"><span>Next</span> &#187;</a>
+<ul id="pageTrack">
+	<li><a href="#" id="button1" class="button"> </a> </li>
+	<li><a href="#" id="button2" class="button"> </a> </li>
+	<li><a href="#" id="button3" class="button"> </a> </li>
+	<li><a href="#" id="button4" class="button"> </a> </li>
+	<li><a href="#" id="button5" class="button"> </a> </li>
+	<li><a href="#" id="button6" class="button"> </a> </li>
+	<li><a href="#" id="button7" class="button"> </a> </li>
+</ul>
+
+</div>
+
+<div id="view">
+<div id="container">
+
+<div id="slide1" class="slide">
+	<h2>BizBuilders Networking Groups</h2>
+	<p>Making introductions and building relationships primarily through weekly meetings and passing referrals.</p>
+	<p>Whether you have a new business or an established business, <strong>networking</strong> is the most certain method for finding and creating new clients and increasing your business.</p>
+</div>	
+
+<div id="slide2" class="slide">
+	<h2>Bizbuilders is a Referral Club</h2>
+	<p>Everyone has business contacts, friends, and family who know, like, and trust you. These are the people to whom you can sell your products and services.</p>
+	<p>The key is to broaden and deepen these contacts. Bizbuilders allows you to grow this market faster, by using the contacts of others.</p>
+	<p class="small">You build relationships with other professionals in you group and they introduce you to people who they know, like, and trust them and need your products and services.</p>
+	<p class="small">You have just expanded your circle of influence, your contacts, and your customer base.</p>
+</div>
+
+<div id="slide3" class="slide">
+	<h2>Meetings</h2>
+	<p>Meetings follow an organized itinerary with at least the following elements:</p>
+	<ul>
+		<li>Welcome and introduction</li>
+		<li>Guest introductions</li>
+		<li>Individual commercials</li>
+		<li>Passing of referrals</li>
+		<li>Special presentations--this is an ongoing personal and professional development program</li>
+	</ul>
+	<p>The order is <strong>not</strong> important--just the ingredients.</p>
+	<p>We start on time, end on time, and have a good time!</p>
+</div>
+
+<div id="slide4" class="slide">
+	<h2>Organizational Structure</h2>
+	<p>The Chair is responsible for conducting the meeting as well as heading up the membership committee.</p>
+	<p>The Secretary is responsible for keeping track of chapter statistics and dues payments as well as membership committee motions and decisions.</p>
+</div>
+
+<div id="slide5" class="slide">
+	<h2>Membership Requirements</h2>
+	<ul>
+		<li>Agreeing to support the professionals in your group to the best of your ability by actively pursuing business on their behalf with your own contacts.</li>
+		<li>Agreeing to invite other business professionals to join your chapter and help the group grow.</li>
+		<li>Agreeing to keep your business practices to the highest ethical and professional standards of your industry.</li>
+		<li class="small">Further agreeing to conduct yourself as an ambassador of BizBuilders in a respectfule, polite, and courteous manner.</li>
+		<li class="small">Attending all meetings and assign a substitute to attend when you have a non-emergencey absence.</li>
+	</ul>
+</div>
+
+<div id="slide6" class="slide">
+	<h2>Bylaws</h2>
+	<p>Groups must qualify for chaptering in BizBuilders with the following requirements:</p>
+	<ul>
+		<li>Three consecutive weeks with more than 3 professionals in attendance.</li>
+		<li>Three complteted membership applications with dues and business cards.</li>
+		<li>The formal appointment of a chair and secretary.</li>
+		<li>In order to maintain exclusivity and high quality, membership in BizBuilders Groups is subject to review and subsequent acceptance or denial by the group and Bizbuilders Corporate.</li>
+		<li>Groups may establish membership committees to review applications and interviews of prospective members before presenting their findings to the whole group for a vote.</li>
+		<li class="small">Members missing over three consecutive meetings can be replaced with someone else in that industry. Usually this is done by mutual consent.</li>
+		<li class="small">Members are encouraged to commission a substitute if they know they will be absent.</li>
+	</ul>
+</div>
+
+<div id="slide7" class="slide">
+	<h2>BizBuilders Networking Groups</h2>
+	<p>Members enjoy access to the entire organization, including online calendar of events, member forums, and regional leadership events and conferences.</p>
+	<p>BizBuilder's goal is to establish 500 groups of 30 professionals each in the West and around the Pacific Rim</p>
+	<p>To join, or to start a new chapter, please <a href="contact.php">contact us</a>.</p>
+</div>
+
+
+</div><!-- end #container -->
+</div><!-- end #view -->
+</body>
+</html>
